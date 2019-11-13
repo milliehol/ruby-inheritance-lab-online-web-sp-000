60674ba15900a76f56039bd5f220cf7d8a32c901
Carla Holcomb <carla.programmer@gmail.com>
2019-11-13 03:25:03 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,5 @@
 class User
    attr_accessor :fName, :lName
 
-  def initialize(fName, lName)
-    @first_name = fName
-    @last_name = lName
-
-  end
-
-  def first_name
-   @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
 
 end
