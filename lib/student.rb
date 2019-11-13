12275309ee5961060909bf60e28d2235c066337a
Carla Holcomb <carla.programmer@gmail.com>
2019-11-13 03:09:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student 
 
+  def initialize
+
+    @knowledge = []
+
+  end
+
+  def first_name(fName)
+    @first_name = fName
+     
+  end
+
+  def last_name(lName)
+    @last_name = lName
+     
+  end
+
 end
