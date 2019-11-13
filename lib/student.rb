85e28158369f9bbae41b908b3e7928cd1971d6b3
Carla Holcomb <carla.programmer@gmail.com>
2019-11-13 03:09:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,4 @@
-class Student 
+class Student
 
   def initialize
 
@@ -8,12 +8,12 @@ class Student
 
   def first_name(fName)
     @first_name = fName
-     
+
   end
 
   def last_name(lName)
     @last_name = lName
-     
+
   end
 
 end
